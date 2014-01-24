e71c43ce6b8952e3e523df382272ab8c897c11d8
Róbert Selvek <robert.selvek@gmail.com>
2014-01-24 17:07:00 +0100

    Improved styling

--- error.js
+++ error.js
@@ -1,7 +1,7 @@
 exports.error = function(errView) {
 	return function(req, res, next) {
 		res.error = function(error){
-			res.status(500).render(errView, {title: "Chyba!", error: error})
+			res.status(500).render(errView, {title: "Nastala chyba", error: error})
 		}
 
 		next();
--- routes/exam.js
+++ routes/exam.js
@@ -1,37 +1,42 @@
 var db = require("../models")
-	, moment = require("moment")
-	, _ = require("lodash");
+    , moment = require("moment")
+    , _ = require("lodash");
 
 exports.list = function(req, res) {
     var d = new Date()
-	db.Exam.findAll({
-		where: ["date >= ?", moment().format("YYYY-MM-DD")],
-		order: "date ASC",
-		include: [db.Subject]
-	}).success(function(exams) {
-		_.forEach(exams, function(exam) {
-			time = moment(exam.date).lang("sk").calendar();
-			exam.textTime = time.substring(0, time.length-7)
-		});
+    db.Exam.findAll({
+        where: ["date >= ?", moment().format("YYYY-MM-DD")],
+        order: "date ASC",
+        include: [db.Subject]
+    }).success(function(exams) {
+        _.forEach(exams, function(exam) {
+            var time = moment(exam.date).lang("sk").calendar();
+            var match = /.*( o \d?\d:\d?\d)/.exec(time);
+            if (match === null) {
+                exam.textTime = time;
+            } else {
+                exam.textTime = time.substr(0, time.length-match[1].length)
+            }
+        });
         res.render("exams", {title: "Zoznam písomiek", exams: exams});
     }).error(res.error);
 }
 
 exports.add = function(req, res) {
-	db.Subject.findAll({attributes: ["id", "name"]}).success(function(subjects){
-		res.render("add-exam", {title: "Pridať písomku", subjects: subjects});
-	}).error(res.error);
+    db.Subject.findAll({attributes: ["id", "name"]}).success(function(subjects){
+        res.render("add-exam", {title: "Pridať písomku", subjects: subjects});
+    }).error(res.error);
 }
 
 exports.submit = function(req, res) {
-	console.log("Telo predmetu:" + JSON.stringify(req.body));
-	db.Exam.create(req.body).success(function () {
-		res.redirect("/");
-	}).error(res.error);
+    console.log("Telo predmetu:" + JSON.stringify(req.body));
+    db.Exam.create(req.body).success(function () {
+        res.redirect("/");
+    }).error(res.error);
 }
 
 exports.delete = function(req, res) {
-	db.Exam.destroy({id: req.params.id}).success(function() {
-		res.redirect("/exam");
-	}).error(res.error);
+    db.Exam.destroy({id: req.params.id}).success(function() {
+        res.redirect("/exam");
+    }).error(res.error);
 }
